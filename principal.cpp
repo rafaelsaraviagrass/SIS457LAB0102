65ca54440beab0affcefb1be2be8f82cc15f4fac
rafaelsaraviagrass <90355634+rafaelsaraviagrass@users.noreply.github.com>
2022-04-05 00:38:13 -0400

    Update principal.cpp

---
+++ principal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	std::cout << "Nombre Completo: Rafael Alejandro Saravia Grass" << std::endl;
+	cout << "Edad: 19 años" << endl;
+	cout << "Carrera: Ingenieria de sistemas" << endl;
+	cout << "Altura: 1.80 mts" << endl;
+	cout << "Peso: 62 kg" << endl;
+	cout << "Nacimiento: 15 de abril del 2002" << endl;
+	cout << "Apodo: rafa" << endl;
+	cout << "Comida favorita: Keperi y lasagnna" << endl;
+	cout << "Pasatiempos: Musica, leer, entrenar" << endl;
+	return 0;
+}
